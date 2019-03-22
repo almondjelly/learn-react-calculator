e0ef7b90674d59086d89380d962860c1a5d26f81
almondjelly <arelaxingsaturdayafternoon@gmail.com>
2019-03-22 11:05:09 -0700

    Set up test for Calculator component; add state to Calculator

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 class Calculator extends Component {
+  state = {
+    displayValue: '0',
+    numbers: [],
+    operators: [],
+    selectedOperator: '',
+    storedValue: '',
+  }
+
   render() {
     return (
       <div>
---
+++ src/components/Calculator/Calculator.spec.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { shallow } from 'enzyme';
+import Calculator from './Calculator';
+
+describe('Calculator', () => {
+  let wrapper;
+  beforeEach(() => wrapper = shallow(<Calculator />));
+  
+  it('should render a <div />', () => {
+    expect(wrapper.find('div').length).toEqual(1);
+  });
+});

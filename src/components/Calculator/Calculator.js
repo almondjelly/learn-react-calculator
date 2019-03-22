bc3481b7547e1571f7ddf0c5ee9f9640f0e112ae
almondjelly <arelaxingsaturdayafternoon@gmail.com>
2019-03-22 11:21:58 -0700

    Add functions to Calculator

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 
 class Calculator extends Component {
   state = {
-    displayValue: '0',
-    numbers: [],
-    operators: [],
-    selectedOperator: '',
-    storedValue: '',
+    displayValue: '0',    // inputed or computed value to be displayed in <Display />
+    numbers: [],          // array of strings used for the number keys in <Keys />
+    operators: [],        // array of strings used for the operator keys in <Keys />
+    selectedOperator: '', // operator selected for math operation held in memory
+    storedValue: '',      // inputed or computed value to use for math operation
+  }
+
+  // Updates and renders the displayed value to the DOM
+  updateDisplay = () => {
+    console.log('update display');
+  }
+
+  // Saves the chosen operator to the component state object
+  setOperator = () => {
+    console.log('set operation');
+  }
+
+  // Handles math operations
+  callOperator = () => {
+    console.log('call operation');
   }
 
   render() {
     return (
-      <div>
-        
-      </div>
+      <div className="calculator-container" />
     );
   }
 }

32fa815ce14541d6d16846e65f65cfd211c6bdd4
almondjelly <arelaxingsaturdayafternoon@gmail.com>
2019-03-24 21:10:42 -0700

    Write tests for updating calculator display

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -35,3 +35,49 @@ describe('Calculator', () => {
   });
 
 });
+
+describe('updateDisplay', () => {
+  let wrapper;
+  beforeEach(() => wrapper = shallow(<Calculator />));
+
+  it('updates displayValue', () => {
+    wrapper.instance().updateDisplay('2');
+    expect(wrapper.state('displayValue')).toEqual('2');
+  });
+
+  it('concatenates displayValue', () => {
+    wrapper.instance().updateDisplay('2');
+    wrapper.instance().updateDisplay('4');
+    expect(wrapper.state('displayValue')).toEqual('24');
+  });
+
+  it('removes leading zero from displayValue', () => {
+    wrapper.instance().updateDisplay('0');
+    wrapper.instance().updateDisplay('2');
+    expect(wrapper.state('displayValue')).toEqual('2');
+  });
+
+  it('prevents multiple leading zeros from displayValue', () => {
+    wrapper.instance().updateDisplay('0');
+    wrapper.instance().updateDisplay('0');
+    expect(wrapper.state('displayValue')).toEqual('0');
+  });
+
+  it('clears the last character of displayValue', () => {
+    wrapper.instance().updateDisplay('2');
+    wrapper.instance().updateDisplay('4');
+    wrapper.instance().updateDisplay('ce');
+    expect(wrapper.state('displayValue')).toEqual('2');
+  });
+
+  it('prevents multiple instances of decimal in displayValue', () => {
+    wrapper.instance().updateDisplay('.');
+    wrapper.instance().updateDisplay('.');
+    expect(wrapper.state('displayValue')).toEqual('.');
+  });
+
+  it('sets displayValue to zero if displayValue is an empty string', () => {
+    wrapper.instance.updateDisplay('ce');
+    expect(wrapper.state('displayValyue')).toEqual('0');
+  });
+});

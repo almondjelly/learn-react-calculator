c2133778fc2c0b228db426355a7c4c3d59556de8
almondjelly <arelaxingsaturdayafternoon@gmail.com>
2019-03-24 21:00:08 -0700

    Refactor Keypad tests

--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Keypad from './Keypad';
 
 describe('Keypad', () => {
@@ -14,22 +14,47 @@ describe('Keypad', () => {
     />
   ));
 
+  it('should render correctly', () =>
+    expect(wrapper).toMatchSnapshot()
+  );
+
   it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(3);
+    expect(wrapper.find('div').length).toEqual(4);
   });
 
-  it('should render the values of numbers', () => {
+  it('should render an instance of the Key component for each index of numbers, operators, and the submit key', () => {
+    const numbers = ['0', '1'];
+    const operators = ['+', '-'];
+    const submit = 1;
+    const keyTotal = numbers.length + operators.length + submit;
+    wrapper.setProps({numbers, operators});
+    expect(wrapper.find('Key').length).toEqual(keyTotal);
+  });
+
+});
+
+describe('mounted Keypad', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(
+      <Keypad 
+        numbers={[]}
+        operators={[]}
+        callOperator={jest.fn()}
+        setOperator={jest.fn()}
+        updateDisplay={jest.fn()}
+      />
+    );
+  });
+
+  it('should render the values of numbers to the DOM', () => {
     wrapper.setProps({numbers: ['0', '1', '2']});
     expect(wrapper.find('.numbers-container').text()).toEqual('012');
   });
 
-  it('should render the values of operators', () => {
+  it('should render the values of operators to the DOM', () => {
     wrapper.setProps({operators: ['+', '-', '*', '/']});
     expect(wrapper.find('.operators-container').text()).toEqual('+-*/');
-  });
-
-  it('should render an instance of the Key component', () => {
-    expect(wrapper.find('Key').length).toEqual(1);
   });
 
 });
